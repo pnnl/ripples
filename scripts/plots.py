2f861d2c7bf10a2a5094f5549981256223c0accc
Minutoli, Marco <marco.minutoli@pnnl.gov>
2018-08-10 17:15:44 -0700

    Improve plots.

--- scripts/plots.py
+++ scripts/plots.py
@@ -20,6 +20,7 @@ def main():
 
     import pandas as pd
     import matplotlib.pyplot as plt
+    import matplotlib.ticker
     import math
     import numpy as np
 
@@ -47,8 +48,16 @@ def main():
         else:
             ax1.plot(dt['NumThreads'], dt[phase], label=phase, color=color)
 
+    l = ax1.get_ylim()
+    l2 = ax2.get_ylim()
+    f = lambda x : l2[0]+(x-l[0])/(l[1]-l[0])*(l2[1]-l2[0])
+    ticks = f(ax1.get_yticks())
+    ax2.yaxis.set_major_locator(matplotlib.ticker.FixedLocator(ticks))
+
     ax1.set_xlabel('Number of Threads')
+    ax1.set_xticks(dt['NumThreads'])
     ax1.set_ylabel('Time (ms)')
+    ax1.grid(True)
 
     h1, l1 = ax1.get_legend_handles_labels()
     h2, l2 = ax2.get_legend_handles_labels()
@@ -60,17 +69,21 @@ def main():
     x = dt['NumThreads']
     width = 0.35
 
-    plots = []
+    fig, ax = plt.subplots()
     bottom = dt[phases[0]]
     for i, phase in enumerate(phases):
         c = colors[i]
         if i is 0:
-            plots.append(plt.bar(x, dt[phase], width, color=c))
+            ax.bar(x, dt[phase], width, color=c, label=phase)
         else:
-            plots.append(plt.bar(x, dt[phase], width, bottom=bottom, color=c))
+            ax.bar(x, dt[phase], width, bottom=bottom, color=c, label=phase)
             bottom += dt[phase]
 
-    plt.legend(plots, phases)
+    ax.grid(True)
+    ax.set_xticks(x)
+    ax.set_xlabel(x)
+    h1, l1 = ax.get_legend_handles_labels()
+    plt.legend(h1, l1)
     plt.ylabel('Time (ms)')
     plt.xlabel('Number of Threads')
     plt.savefig(args.input_file[0] + '.2.pdf', bbox_inches='tight')
