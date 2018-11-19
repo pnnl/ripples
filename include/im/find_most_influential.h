370e384c14b15250971abbda2de8b25624e7400e
Minutoli, Marco <marco.minutoli@pnnl.gov>
2018-11-19 13:01:12 -0800

    Breaking in functions FindMostInfluentialSet.

--- include/im/find_most_influential.h
+++ include/im/find_most_influential.h
@@ -17,6 +17,163 @@
 
 namespace im {
 
+//! \brief Count the occurrencies of vertices in the RRR sets.
+//!
+//! \tparam InItr The input sequence iterator type.
+//! \tparam OutItr The output sequence iterator type.
+//!
+//! \param in_begin The begin of the sequence of RRR sets.
+//! \param in_end The end of the sequence of RRR sets.
+//! \param out_begin The begin of the sequence storing the counters for each
+//! vertex.
+//! \param out_end The end of the sequence storing the counters for each vertex.
+//! \param e The execution policy tag.
+template <typename InItr, typename OutItr>
+void CountOccurrencies(InItr in_begin, InItr in_end, OutItr out_begin,
+                       OutItr out_end, sequential_tag &&e) {
+  using rrr_set_type = typename std::iterator_traits<InItr>::value_type;
+  using vertex_type = typename rrr_set_type::value_type;
+  for (; in_begin != in_end; ++in_begin) {
+    std::for_each(in_begin->begin(), in_begin->end(),
+                  [&](const vertex_type v) { *(out_begin + v) += 1; });
+  }
+}
+
+//! \brief Count the occurrencies of vertices in the RRR sets.
+//!
+//! \tparam InItr The input sequence iterator type.
+//! \tparam OutItr The output sequence iterator type.
+//!
+//! \param in_begin The begin of the sequence of RRR sets.
+//! \param in_end The end of the sequence of RRR sets.
+//! \param out_begin The begin of the sequence storing the counters for each
+//! vertex.
+//! \param out_end The end of the sequence storing the counters for each vertex.
+//! \param e The execution policy tag.
+template <typename InItr, typename OutItr>
+void CountOccurrencies(InItr in_begin, InItr in_end, OutItr out_begin,
+                       OutItr out_end, omp_parallel_tag &&) {
+  using rrr_set_type = typename std::iterator_traits<InItr>::value_type;
+  using vertex_type = typename rrr_set_type::value_type;
+
+#pragma omp parallel
+  {
+    size_t num_elements = std::distance(out_begin, out_end);
+    size_t threadnum = omp_get_thread_num(), numthreads = omp_get_num_threads();
+    vertex_type low = num_elements * threadnum / numthreads,
+                high = num_elements * (threadnum + 1) / numthreads;
+
+    for (; in_begin != in_end; ++in_begin) {
+      auto begin = std::lower_bound(in_begin->begin(), in_begin->end(), low);
+      auto end = std::upper_bound(in_begin->begin(), in_begin->end(), high - 1);
+      std::for_each(begin, end,
+                    [&](const vertex_type v) { *(out_begin + v) += 1; });
+    }
+  }
+}
+
+//! \brief Initialize the Heap storage.
+//!
+//! \tparam InItr The input sequence iterator type.
+//! \tparam OutItr The output sequence iterator type.
+//!
+//! \param in_begin The begin of the sequence of vertex counters.
+//! \param in_end The end of the sequence of vertex counters.
+//! \param out_begin The begin of the sequence used as storage in the Heap.
+//! \param out_end The end of the sequence used as storage in the Heap.
+//! \param e The execution policy tag.
+template <typename InItr, typename OutItr>
+void InitHeapStorage(InItr in_begin, InItr in_end, OutItr out_begin,
+                     OutItr out_end, sequential_tag &&) {
+  using value_type = typename std::iterator_traits<OutItr>::value_type;
+  using vertex_type = typename value_type::first_type;
+
+  for (vertex_type v = 0; in_begin != in_end; ++in_begin, ++v, ++out_begin) {
+    *out_begin = {v, *in_begin};
+  }
+}
+
+//! \brief Initialize the Heap storage.
+//!
+//! \tparam InItr The input sequence iterator type.
+//! \tparam OutItr The output sequence iterator type.
+//!
+//! \param in_begin The begin of the sequence of vertex counters.
+//! \param in_end The end of the sequence of vertex counters.
+//! \param out_begin The begin of the sequence used as storage in the Heap.
+//! \param out_end The end of the sequence used as storage in the Heap.
+//! \param e The execution policy tag.
+template <typename InItr, typename OutItr>
+void InitHeapStorage(InItr in_begin, InItr in_end, OutItr out_begin,
+                     OutItr out_end, omp_parallel_tag &&) {
+  using value_type = typename std::iterator_traits<OutItr>::value_type;
+  using vertex_type = typename value_type::first_type;
+
+#pragma omp parallel for
+  for (vertex_type v = 0; v < std::distance(in_begin, in_end); ++v) {
+    *(out_begin + v) = {v, *(in_begin + v)};
+  }
+}
+
+//! \brief Update the coverage counters.
+//!
+//! \tparam VertexTy The type of the vertices.
+//! \tparam RRRsetsTy The type storing RRR sets.
+//! \tparam RemovedVectorTy The type of the vector storing removed RRR sets.
+//! \tparam VertexCoverageVectorTy The type of the vector storing counters.
+//!
+//! \param v The chosen vertex.
+//! \param RRRsets The sequence of RRRsets.
+//! \param removed The vector storing covered/uncovered flags for the RRRsets.
+//! \param vertexCoverage The vector storing the counters to be updated.
+template <typename VertexTy, typename RRRsetsTy, typename RemovedVectorTy,
+          typename VertexCoverageVectorTy>
+void UpdateCounters(const VertexTy v, const RRRsetsTy &RRRsets,
+                    RemovedVectorTy &removed,
+                    VertexCoverageVectorTy &vertexCoverage, sequential_tag &&) {
+  for (size_t i = 0; i < RRRsets.size(); ++i) {
+    if (removed[i]) continue;
+
+    if (std::binary_search(RRRsets[i].begin(), RRRsets[i].end(), v)) {
+      removed[i] = true;
+      for (size_t j = 0; j < RRRsets[i].size(); ++j) {
+        vertexCoverage[RRRsets[i][j]] -= 1;
+      }
+    }
+  }
+}
+
+//! \brief Update the coverage counters.
+//!
+//! \tparam VertexTy The type of the vertices.
+//! \tparam RRRsetsTy The type storing RRR sets.
+//! \tparam RemovedVectorTy The type of the vector storing removed RRR sets.
+//! \tparam VertexCoverageVectorTy The type of the vector storing counters.
+//!
+//! \param v The chosen vertex.
+//! \param RRRsets The sequence of RRRsets.
+//! \param removed The vector storing covered/uncovered flags for the RRRsets.
+//! \param vertexCoverage The vector storing the counters to be updated.
+template <typename VertexTy, typename RRRsetsTy, typename RemovedVectorTy,
+          typename VertexCoverageVectorTy>
+void UpdateCounters(const VertexTy v, const RRRsetsTy &RRRsets,
+                    RemovedVectorTy &removed,
+                    VertexCoverageVectorTy &vertexCoverage,
+                    omp_parallel_tag &&) {
+  for (size_t i = 0; i < RRRsets.size(); ++i) {
+    if (removed[i]) continue;
+
+    if (std::binary_search(RRRsets[i].begin(), RRRsets[i].end(), v)) {
+      removed[i] = true;
+
+#pragma omp parallel for
+      for (size_t j = 0; j < RRRsets[i].size(); ++j) {
+        vertexCoverage[RRRsets[i][j]] -= 1;
+      }
+    }
+  }
+}
+
 //! \brief Select k seeds starting from the a list of Random Reverse
 //! Reachability Sets.
 //!
@@ -33,7 +190,7 @@ namespace im {
 template <typename GraphTy, typename RRRset, typename execution_tag>
 auto FindMostInfluentialSet(const GraphTy &G, size_t k,
                             const std::vector<RRRset> &RRRsets,
-                            execution_tag &&) {
+                            execution_tag &&ex_tag) {
   using vertex_type = typename GraphTy::vertex_type;
 
   std::vector<uint32_t> vertexCoverage(G.num_nodes(), 0);
@@ -49,36 +206,12 @@ auto FindMostInfluentialSet(const GraphTy &G, size_t k,
 
   std::vector<std::pair<vertex_type, size_t>> queue_storage(G.num_nodes());
 
-  if (std::is_same<execution_tag, omp_parallel_tag>::value) {
-#pragma omp parallel
-    {
-      size_t threadnum = omp_get_thread_num(),
-             numthreads = omp_get_num_threads();
-      vertex_type low = G.num_nodes() * threadnum / numthreads,
-                  high = G.num_nodes() * (threadnum + 1) / numthreads;
+  CountOccurrencies(RRRsets.begin(), RRRsets.end(), vertexCoverage.begin(),
+                    vertexCoverage.end(), std::forward<execution_tag>(ex_tag));
 
-      for (size_t i = 0; i < RRRsets.size(); ++i) {
-        auto begin =
-            std::lower_bound(RRRsets[i].begin(), RRRsets[i].end(), low);
-        auto end =
-            std::upper_bound(RRRsets[i].begin(), RRRsets[i].end(), high - 1);
-        std::for_each(begin, end,
-                      [&](const vertex_type v) { vertexCoverage[v] += 1; });
-      }
-    }
-#pragma omp parallel for
-    for (vertex_type i = 0; i < G.num_nodes(); ++i) {
-      queue_storage[i] = std::make_pair(i, vertexCoverage[i]);
-    }
-  } else {
-    for (size_t i = 0; i < RRRsets.size(); ++i) {
-      std::for_each(RRRsets[i].begin(), RRRsets[i].end(),
-                    [&](const vertex_type v) { vertexCoverage[v] += 1; });
-    }
-    for (vertex_type i = 0; i < G.num_nodes(); ++i) {
-      queue_storage[i] = std::make_pair(i, vertexCoverage[i]);
-    }
-  }
+  InitHeapStorage(vertexCoverage.begin(), vertexCoverage.end(),
+                  queue_storage.begin(), queue_storage.end(),
+                  std::forward<execution_tag>(ex_tag));
 
   priorityQueue queue(cmp, std::move(queue_storage));
 
@@ -100,32 +233,8 @@ auto FindMostInfluentialSet(const GraphTy &G, size_t k,
 
     uncovered -= element.second;
 
-    if (std::is_same<execution_tag, omp_parallel_tag>::value) {
-      for (size_t i = 0; i < RRRsets.size(); ++i) {
-        if (removed[i]) continue;
-
-        if (std::binary_search(RRRsets[i].begin(), RRRsets[i].end(),
-                               element.first)) {
-          removed[i] = true;
-#pragma omp parallel for
-          for (size_t j = 0; j < RRRsets[i].size(); ++j) {
-            vertexCoverage[RRRsets[i][j]] -= 1;
-          }
-        }
-      }
-    } else {
-      for (size_t i = 0; i < RRRsets.size(); ++i) {
-        if (removed[i]) continue;
-
-        if (std::binary_search(RRRsets[i].begin(), RRRsets[i].end(),
-                               element.first)) {
-          removed[i] = true;
-          for (size_t j = 0; j < RRRsets[i].size(); ++j) {
-            vertexCoverage[RRRsets[i][j]] -= 1;
-          }
-        }
-      }
-    }
+    UpdateCounters(element.first, RRRsets, removed, vertexCoverage,
+                   std::forward<execution_tag>(ex_tag));
 
     result.push_back(element.first);
   }
@@ -135,4 +244,4 @@ auto FindMostInfluentialSet(const GraphTy &G, size_t k,
 
 }  // namespace im
 
-#endif // IM_FIND_MOST_INFLUENTIAL_H
+#endif  // IM_FIND_MOST_INFLUENTIAL_H
