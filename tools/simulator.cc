3346210df9aa4c78c3be49044b912c9244a8b8b2
Minutoli, Marco <marco.minutoli@pnnl.gov>
2019-03-20 13:16:28 -0700

    Update the simulator simplifying the output.

--- tools/simulator.cc
+++ tools/simulator.cc
@@ -27,7 +27,6 @@ struct SimulatorConfiguration {
   std::string EFileName;
   std::string diffusionModel;
   std::size_t Replicas;
-  std::size_t Tries;
 
   void addCmdOptions(CLI::App &app) {
     app.add_option(
@@ -35,15 +34,8 @@ struct SimulatorConfiguration {
         "The file storing the experiments form a run of an inf-max algorithm.")
         ->group("Simulator Options")
         ->required();
-    app.add_option("-d,--diffusion-model", diffusionModel,
-                   "The diffusion process to simulate on the input network.")
-        ->group("Simulator Options")
-        ->required();
     app.add_option("--replicas", Replicas,
                    "The number of experimental replicas.")
-        ->group("Simulator Options")
-        ->required();
-    app.add_option("--tries", Tries, "The number of tries for each replica.")
         ->group("Simulator Options")
         ->required();
   }
@@ -79,26 +71,26 @@ int main(int argc, char **argv) {
   weightGen.seed(0UL);
   weightGen.split(2, 0);
 
-  using Graph = im::Graph<uint32_t, float>;
-  auto console = spdlog::stdout_color_st("console");
-  console->info("Loading...");
-  Graph G = im::loadGraph<Graph>(CFG, weightGen);
-  console->info("Loading Done!");
-  console->info("Number of Nodes : {}", G.num_nodes());
-  console->info("Number of Edges : {}", G.num_edges());
-
   nlohmann::json experimentRecord;
 
   std::ifstream experimentRecordIS(CFG.EFileName);
 
   experimentRecordIS >> experimentRecord;
+  CFG.diffusionModel = experimentRecord[0]["DiffusionModel"];
+
+  using Graph = im::Graph<uint32_t, float>;
+  auto console = spdlog::stdout_color_st("console");
+  console->info("Loading ...");
+  Graph G = im::loadGraph<Graph>(CFG, weightGen);
+  console->info("Loading Done!");
+  console->info("Number of Nodes : {}", G.num_nodes());
+  console->info("Number of Edges : {}", G.num_edges());
 
   nlohmann::json simRecordLog;
   for (auto &record : experimentRecord) {
     using vertex_type = typename Graph::vertex_type;
 
-    std::vector<std::vector<std::pair<size_t, size_t>>> experiments(
-        CFG.Replicas, std::vector<std::pair<size_t, size_t>>(CFG.Tries));
+    std::vector<std::pair<size_t, size_t>> experiments(CFG.Replicas);
 
     std::vector<vertex_type> seeds = record["Seeds"];
 
@@ -116,26 +108,22 @@ int main(int argc, char **argv) {
                                             omp_get_thread_num());
     }
 
-    for (auto itr = seeds.begin() + 1; itr <= seeds.end(); ++itr) {
 #pragma omp parallel for schedule(dynamic)
       for (size_t i = 0; i < experiments.size(); ++i) {
-        for (auto &v : experiments[i]) {
-          if (CFG.diffusionModel == "IC") {
-            v = simulate(G, seeds.begin(), itr,
-                         generator[omp_get_thread_num()],
-                         im::independent_cascade_tag{});
-          } else if (CFG.diffusionModel == "LT") {
-            v = simulate(G, seeds.begin(), itr,
-                         generator[omp_get_thread_num()],
-                         im::linear_threshold_tag{});
-          } else {
-            throw std::string("Not Yet Implemented");
-          }
+        if (CFG.diffusionModel == "IC") {
+          experiments[i] = simulate(G, seeds.begin(), seeds.end(),
+                                    generator[omp_get_thread_num()],
+                                    im::independent_cascade_tag{});
+        } else if (CFG.diffusionModel == "LT") {
+          experiments[i] = simulate(G, seeds.begin(), seeds.end(),
+                                    generator[omp_get_thread_num()],
+                                    im::linear_threshold_tag{});
+        } else {
+          throw std::string("Not Yet Implemented");
         }
       }
       simRecordLog.push_back(
-          im::GetExperimentRecord(CFG, std::distance(seeds.begin(), itr), record["Epsilon"], experiments));
-    }
+          im::GetExperimentRecord(CFG, std::distance(seeds.begin(), seeds.end()), record["Epsilon"], experiments));
   }
   simRecord->info("{}", simRecordLog.dump(2));
 
