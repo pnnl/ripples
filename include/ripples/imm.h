66f4d5942970fe28ba1c18a6da587763c3b6cf45
Marco Minutoli <marco.minutoli@pnnl.gov>
2021-05-26 18:33:35 -0700

    Make k at most the size of the graph divided by 2 to cover a corner case in the algorithm working with communities

--- include/ripples/imm.h
+++ include/ripples/imm.h
@@ -121,6 +121,7 @@ inline double logBinomial(size_t n, size_t k) {
 template <typename execution_tag>
 ssize_t ThetaPrime(ssize_t x, double epsilonPrime, double l, size_t k,
                    size_t num_nodes, execution_tag &&) {
+  k = std::min(k, num_nodes/2);
   return (2 + 2. / 3. * epsilonPrime) *
          (l * std::log(num_nodes) + logBinomial(num_nodes, k) +
           std::log(std::log2(num_nodes))) *
@@ -138,6 +139,7 @@ inline size_t Theta(double epsilon, double l, size_t k, double LB,
                     size_t num_nodes) {
   if (LB == 0) return 0;
 
+  k = std::min(k, num_nodes/2);
   double term1 = 0.6321205588285577;  // 1 - 1/e
   double alpha = sqrt(l * std::log(num_nodes) + std::log(2));
   double beta = sqrt(term1 * (logBinomial(num_nodes, k) +
