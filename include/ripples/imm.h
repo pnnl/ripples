7a5035be6a9dd5cb6d029fb993b4b918947bffad
Keita Iwabuchi <iwabuchi1@llnl.gov>
2021-06-17 03:36:09 -0700

    Bugfix in the Metall mode:
    Use metall::container::vector instead std::vector
    
    Destruct the Metall manager object after destructing the std::vector<RRRset<GraphTy>> object by utilizing the static local variable mechanism

--- include/ripples/imm.h
+++ include/ripples/imm.h
@@ -182,8 +182,7 @@ auto Sampling(const GraphTy &G, const ConfTy &CFG, double l,
   #if defined ENABLE_MEMKIND
   RRRsetAllocator<vertex_type> allocator(libmemkind::kinds::DAX_KMEM_PREFERRED);
   #elif defined ENABLE_METALL
-  metall::manager manager(metall::open_only, "/tmp/ripples");
-  RRRsetAllocator<vertex_type> allocator = manager.get_allocator();
+  RRRsetAllocator<vertex_type> allocator =  metall_manager_instance().get_allocator();
   #else
   RRRsetAllocator<vertex_type> allocator;
   #endif
@@ -269,9 +268,8 @@ auto Sampling(const GraphTy &G, const ConfTy &CFG, double l,
   #if defined ENABLE_MEMKIND
   RRRsetAllocator<vertex_type> allocator(libmemkind::kinds::DAX_KMEM_PREFERRED);
   #elif defined ENABLE_METALL
-  metall::manager manager(metall::open_only, "/tmp/ripples");
-  RRRsetAllocator<vertex_type> allocator = manager.get_allocator();
-  #else
+  RRRsetAllocator<vertex_type> allocator =  metall_manager_instance().get_allocator();
+#else
   RRRsetAllocator<vertex_type> allocator;
   #endif
   std::vector<RRRset<GraphTy>> RR;
@@ -359,12 +357,6 @@ auto IMM(const GraphTy &G, const ConfTy &CFG, double l, PRNG &gen,
   size_t k = CFG.k;
   double epsilon = CFG.epsilon;
 
-  #if defined ENABLE_METALL
-  {
-    metall::manager _(metall::create_only, "/tmp/ripples");
-  }
-  #endif
-
   std::vector<trng::lcg64> generator(1, gen);
 
   l = l * (1 + 1 / std::log2(G.num_nodes()));
@@ -412,12 +404,6 @@ auto IMM(const GraphTy &G, const ConfTy &CFG, double l, GeneratorTy &gen,
   size_t k = CFG.k;
   double epsilon = CFG.epsilon;
   auto &record(gen.execution_record());
-
-  #if defined ENABLE_METALL
-  {
-    metall::manager _(metall::create_only, "/tmp/ripples");
-  }
-  #endif
 
   l = l * (1 + 1 / std::log2(G.num_nodes()));
 
