c13084497397450ee77ea997dde11b6652c79087
marco.minutoli@pnnl.gov <minu455@we48159.lan>
2023-09-28 23:31:33 -0700

    Add a nix flake that builds the development environment.

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "A Nix-flake-based Ripples development environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [cmake clang_15 mpi conan] ++
+            (with pkgs.llvmPackages_15; [stdenv openmp]);
+        };
+      });
+    };
+}
