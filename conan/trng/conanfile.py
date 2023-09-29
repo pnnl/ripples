f3348b392a6eeddadf237e8a2577c7146b45a0dc
marco.minutoli@pnnl.gov <minu455@we48159.lan>
2023-09-28 23:33:25 -0700

    Move TRNG recipe to conan2.

--- conan/trng/conanfile.py
+++ conan/trng/conanfile.py
@@ -1,37 +1,41 @@
 #!/usr/bin/env python
 
-from conans import ConanFile, CMake, tools
+import os
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
+from conan.tools.files import get, replace_in_file
 
 class LibtrngConan(ConanFile):
     name = "libtrng"
     license = "BSD"
-    version = "4.22"
+    version = "4.23.1"
     author = "Heiko Bauke"
-    version= "basic_hip_support"
     description = "Tina's Random Number Generator Library"
     topics = ("Pseudo-Random Number Generator")
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
-    default_options = "shared=True"
-    generators = "cmake"
+    default_options = {"shared" : True}
 
     def source(self):
-        tools.download('https://github.com/rabauke/trng4/archive/refs/tags/v' + self.version + '.tar.gz', 'trng-' + self.version + '.tar.gz')
-        tools.unzip('trng-' + self.version + '.tar.gz')
-        tools.replace_in_file(
-            'trng4-' + self.version + '/CMakeLists.txt',
-            'add_subdirectory(examples)',
-            ''
-        )
-        return 'trng4-' + self.version
+        get(self,
+            'https://github.com/rabauke/trng4/archive/refs/tags/v' + self.version + '.tar.gz',
+            strip_root=True)
+        replace_in_file(self, os.path.join(self.source_folder, "CMakeLists.txt"),
+                        "add_subdirectory(examples)", "")
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
 
     def build(self):
         cmake = CMake(self)
-        cmake.definitions['TRNG_ENABLE_EXAMPLES'] = False
-        cmake.definitions['TRNG_ENABLE_TESTS'] = False
-        cmake.configure(source_folder='trng')
-        cmake.parallel = False
+        cmake.configure()
         cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
         cmake.install()
 
     def package_info(self):
